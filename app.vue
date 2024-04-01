37984676d07c56098c9a7512a0972668dc05618c
Marcela Kramer <marcelabarretokramer10@gmail.com>
2024-04-01 15:56:24 -0300

    create initial page

--- app.vue
+++ app.vue
@@ -1,5 +1,27 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="bg-surface-900 h-screen w-screen flex flex-col justify-center items-center">
+    <h1 class="text-primary-50 font-bold text-6xl">
+      Welcome to Nuxt3 development page!
+    </h1>
+    <p class="text-primary-200 text-center m-10 text-2xl max-w-xl">
+      This is the initial page of a website created to upgrade the Nuxt3 development skills.
+    </p>
+    <div class="flex justify-center items-center gap-3">
+      <Button
+        label="Get started"
+        raised
+      />
+      <Button
+        label="Sign in"
+        severity="secondary"
+        raised
+      />
+    </div>
   </div>
 </template>
+
+<script setup>
+</script>
+
+<style>
+</style>
